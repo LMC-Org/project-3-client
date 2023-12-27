691ce7674656df02ba4c00e8e2fbcd6039c10df8
claratesmon <claraperezmontes@gmail.com>
2023-12-27 20:06:35 +0100

    cloudinary-token middleware

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,20 +1,15 @@
 import { useEffect, useState } from "react";
 import HelpPostHome from "../../components/HelpPostHome/HelpPostHome";
 import "./HomePage.css";
+import service from '../../services/file-upload.service'
 
 function HomePage() {
 	const [helpPostsArr, setHelpPostsArr] = useState([]);
 
-	/* 	const renderHelpPost = (helpPostsArr) => {
-			console.log("in");
-			helpPostsArr.map((eachPost, index) => {
-				console.log("post ", index);
-				return ( <HelpPostHome key="index" />);
-			})
-		 }; */
+
 	const handleSearch = (searchValue) => {
 		const helpPostsUpdated = helpPostsArr.filter((eachHelpPost) => {
-			console.log(eachHelpPost.title)
+			
 			return eachHelpPost.title.includes(searchValue)
 		})
 		if (searchValue === "") {
@@ -46,19 +41,19 @@ function HomePage() {
 			<div id="home-content-wrapper">
 
 				<div className="home-head">
-					<p className="home-title">How can you help today?</p>
+					<p className="home-title">HOW CAN YOU HELP TODAY?</p>
 					<br />
 					<div className="search-bar">
 						<input onChange={(event) => handleSearch(event.target.value)} type="text" name="search-input" id="search-input" />
-						<span> {''} {''}   <i class="fa fa-search"></i></span>
+						<span>{''}    {''}<i className="fa fa-search"></i></span>
 
 
 
 					</div>
 				</div>
 				{
-					helpPostsArr.map((eachPost, index) => {
-						console.log("post ", eachPost);
+					helpPostsArr && helpPostsArr.map((eachPost, index) => {
+						
 						return (
 							<div key={index} className="posts-container">
 
