8cb91543fceccb78a6555e62bac92da66c4af6c5
Martin Palkov <martin.palkov@gmail.com>
2024-01-26 23:30:33 +0100

    progress backup

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,7 +6,6 @@ import { AuthContext } from "../../context/auth.context";
 function Navbar() {
   const { isLoggedIn, user, logOutUser } = useContext(AuthContext);
   const [userData, setUserData] = useState('')
-
   const { userId } = useParams()
   const BACKEND_ROOT = import.meta.env.VITE_SERVER_URL;
   const navigate = useNavigate();
@@ -22,29 +21,22 @@ function Navbar() {
   const hasTokens = async () => {
     console.log(userData.tokens)
     if (userData.tokens < 1) {
-
-
       try {
         await new Promise((resolve) => setTimeout(resolve, 100));
         const banner = document.querySelector(".no-tokens-banner")
         banner.classList.toggle("hidden");
-
         if (banner.style.top === "0px") {
           banner.style.top = "-300px"
-
-
         }
-
       } catch (error) {
         console.log("There was an error:", error)
       }
-
     }
   }
+
+  // Hide the banner after 5 seconds
   const hideBanner = () => {
     const banner = document.querySelector(".no-tokens-banner");
-
-    // Hide the banner after 5 seconds
     setTimeout(() => {
       banner.classList.add("hidden");
     }, 4000);
@@ -55,7 +47,6 @@ function Navbar() {
       fetch(`${BACKEND_ROOT}/user/${user._id}`)
         .then((response) => response.json())
         .then((responseJson) => {
-
           setUserData(responseJson);
         })
         .catch((err) => console.log(err));
@@ -74,6 +65,10 @@ function Navbar() {
               <img className="logo" src="/images/4H-logo-round-green2.svg" alt="" />
             </Link>
             <div className="nav-right">
+			  <svg id="notifications" className="new" width="24" height="24" viewBox="0 0 24 24">
+			  	<title>Notifications</title>
+                <path d="M11.5,22C11.64,22 11.77,22 11.9,21.96C12.55,21.82 13.09,21.38 13.34,20.78C13.44,20.54 13.5,20.27 13.5,20H9.5A2,2 0 0,0 11.5,22M18,10.5C18,7.43 15.86,4.86 13,4.18V3.5A1.5,1.5 0 0,0 11.5,2A1.5,1.5 0 0,0 10,3.5V4.18C7.13,4.86 5,7.43 5,10.5V16L3,18V19H20V18L18,16M19.97,10H21.97C21.82,6.79 20.24,3.97 17.85,2.15L16.42,3.58C18.46,5 19.82,7.35 19.97,10M6.58,3.58L5.15,2.15C2.76,3.97 1.18,6.79 1,10H3C3.18,7.35 4.54,5 6.58,3.58Z"></path>
+              </svg>
               <div className="tokens-state">
               <span className="material-symbols-outlined">
                 stat_0
@@ -117,7 +112,6 @@ function Navbar() {
                     <Link to="/createtestimony">
                       <p onClick={handleSidebar} className="side-element">Create testimony {"   "}
                         {"  "} <i className="fa fa-plus" style={{ color: "#111111" }}></i>
-
                         <i className="fa fa-plus" style={{ color: "#a8ec41" }}></i>
                       </p>
                     </Link>
