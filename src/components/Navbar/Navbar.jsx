8a4cb6e65b3fcbf34f01bb742b4ec650b105bd9b
Martin Palkov <martin.palkov@gmail.com>
2024-01-28 16:53:28 +0100

    create notifications fetch with setinterval

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,6 +4,7 @@ import { useContext, useState, useEffect } from "react";
 import { AuthContext } from "../../context/auth.context";
 
 function Navbar() {
+	let newNotifications = false;
   const { isLoggedIn, user, logOutUser } = useContext(AuthContext);
   const [userData, setUserData] = useState('')
   const { userId } = useParams()
@@ -53,6 +54,34 @@ function Navbar() {
     }
   }, [user]);
 
+  const addClassNewToNotifIcon = () => {
+	const notificationsElement = document.querySelector("#notifications-icon");
+	notificationsElement.classList.add("new");
+  };
+
+  //check if there are new notifications
+  	console.log("call seetInterval");
+	const checkNotificationsLoop = setInterval(() => {
+		console.log("Interval execution");
+		if (user && !newNotifications) {
+			fetch(`${BACKEND_ROOT}/user/check-notifications/${user._id}`)
+				.then((response) => response.json())
+				.then((responseJson) => {console.log("checknotif response: ", responseJson); newNotifications = responseJson.hasNewNotifications
+				if (newNotifications) {
+					addClassNewToNotifIcon();
+					clearInterval(checkNotificationsLoop);
+					console.log("addClass & clearinterval INSIDE");
+				}
+			});
+				
+		}
+		else if (newNotifications) {
+			addClassNewToNotifIcon();
+			clearInterval(checkNotificationsLoop);
+			console.log("addClass & clearinterval OUTSIDE");
+		}
+	}, 15000);
+
   return (
     <div className="navbar-container">
       <div className="no-tokens-banner hidden">
@@ -65,7 +94,7 @@ function Navbar() {
               <img className="logo" src="/images/4H-logo-round-green2.svg" alt="" />
             </Link>
             <div className="nav-right">
-			  <svg id="notifications" className="new" width="24" height="24" viewBox="0 0 24 24">
+			  <svg id="notifications-icon" className="" width="24" height="24" viewBox="0 0 24 24">
 			  	<title>Notifications</title>
                 <path d="M11.5,22C11.64,22 11.77,22 11.9,21.96C12.55,21.82 13.09,21.38 13.34,20.78C13.44,20.54 13.5,20.27 13.5,20H9.5A2,2 0 0,0 11.5,22M18,10.5C18,7.43 15.86,4.86 13,4.18V3.5A1.5,1.5 0 0,0 11.5,2A1.5,1.5 0 0,0 10,3.5V4.18C7.13,4.86 5,7.43 5,10.5V16L3,18V19H20V18L18,16M19.97,10H21.97C21.82,6.79 20.24,3.97 17.85,2.15L16.42,3.58C18.46,5 19.82,7.35 19.97,10M6.58,3.58L5.15,2.15C2.76,3.97 1.18,6.79 1,10H3C3.18,7.35 4.54,5 6.58,3.58Z"></path>
               </svg>
