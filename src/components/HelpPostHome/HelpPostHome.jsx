170c59cee43456100c6b8c49b379f50d84c6f96c
Clara Tesmon <claraperezmontes@gmail.com>
2024-03-26 17:58:32 +0100

    deploy test

--- src/components/HelpPostHome/HelpPostHome.jsx
+++ src/components/HelpPostHome/HelpPostHome.jsx
@@ -3,22 +3,22 @@ import { Link } from "react-router-dom";
 import "./HelpPostHome.css";
 
 const HelpPostHome = (props) => {
-	const {_id, title, location, description, creator} = props.post;
+	const { _id, title, location, description, creator } = props.post;
 
 	return (
 		<Link to={`/help-post/${_id}`} >
-		<div  className="help-post-home">
-			<p className="title">{title}</p>
-			<p className="description">{description}</p>
-			<div className="image-location-container">
-<div className="creator-container">
-			<img className="home-creator-picture" src={creator.profilePicture} alt="" />
-			
-			<p className="creator-name-small">{creator.name}</p>
+			<div className="help-post-home">
+				<p className="title">{title}</p>
+				<p className="description">{description}</p>
+				<div className="image-location-container">
+					<div className="creator-container">
+						<img className="home-creator-picture" src={creator.profilePicture} alt="" />
+
+						<p className="creator-name-small">{creator.name}</p>
+					</div>
+					<p className="location">{location} <i className="fa fa-map-marker"></i></p>
+				</div>
 			</div>
-			<p className="location">{location} <i className="fa fa-map-marker"></i></p>
-			</div>
-		</div>
 		</Link>
 	);
 };
